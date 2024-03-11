deb25efefae7f9fbcccf35f7c62ffa32dc234ad2
sashalorens <58805038+sashalorens@users.noreply.github.com>
2024-03-11 11:10:23 +0200

    Feature/settings file (#2)
    * WIP: create a settings file by default or allow user to pick one
    
    * As a user. I want to be able to use a custom config file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using JeraKeyboard;
+using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace JeraUI
 {
@@ -12,6 +15,7 @@ namespace JeraUI
         /// </summary>
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.NotifyIcon trayIcon = new System.Windows.Forms.NotifyIcon();
+        public readonly IJeraKB jeraKB;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -26,6 +30,23 @@ namespace JeraUI
             base.Dispose(disposing);
         }
 
+        private void InitializeLanguages()
+        {
+            bool hasDefaultValue = false;
+            foreach (InputLanguage c in System.Windows.Forms.InputLanguage.InstalledInputLanguages)
+            {
+                comboBox1.Items.Add(c.Culture.EnglishName);
+                comboBox2.Items.Add(c.Culture.EnglishName);
+
+                if (!hasDefaultValue)
+                {
+                    comboBox1.SelectedItem = c.Culture.EnglishName;
+                    comboBox2.SelectedItem = c.Culture.EnglishName;
+                    hasDefaultValue = true;
+                }
+            }
+        }
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -36,88 +57,168 @@ namespace JeraUI
         {
             components = new System.ComponentModel.Container();
             trayIcon = new NotifyIcon(components);
-            contextMenuStrip1 = new ContextMenuStrip(components);
-            toolStripMenuItem1 = new ToolStripMenuItem();
-            toolStripMenuItem2 = new ToolStripMenuItem();
-            toolStripMenuItem3 = new ToolStripMenuItem();
-            toolStripMenuItem4 = new ToolStripMenuItem();
-            toolStripMenuItem5 = new ToolStripMenuItem();
-            contextMenuStrip1.SuspendLayout();
+            trayMenu = new ContextMenuStrip(components);
+            settingsMenuItem = new ToolStripMenuItem();
+            startMenuItem = new ToolStripMenuItem();
+            pauseMenuItem = new ToolStripMenuItem();
+            restartMenuItem = new ToolStripMenuItem();
+            closeMenuItem = new ToolStripMenuItem();
+            comboBox1 = new ComboBox();
+            labelFrom = new Label();
+            labelTo = new Label();
+            comboBox2 = new ComboBox();
+            openFileDialog1 = new OpenFileDialog();
+            browseButton = new Button();
+            browseInput = new TextBox();
+            trayMenu.SuspendLayout();
             SuspendLayout();
             // 
             // trayIcon
             // 
-            trayIcon.ContextMenuStrip = contextMenuStrip1;
+            trayIcon.ContextMenuStrip = trayMenu;
             trayIcon.Text = "notifyIcon1";
             trayIcon.Visible = true;
             // 
-            // contextMenuStrip1
+            // trayMenu
             // 
-            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5 });
-            contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(117, 114);
-            contextMenuStrip1.Text = "Foo";
-            contextMenuStrip1.Opening += contextMenuStrip1_Opening;
+            trayMenu.Items.AddRange(new ToolStripItem[] { settingsMenuItem, startMenuItem, pauseMenuItem, restartMenuItem, closeMenuItem });
+            trayMenu.Name = "contextMenuStrip1";
+            trayMenu.Size = new Size(117, 114);
+            trayMenu.Text = "Foo";
+            trayMenu.Opening += contextMenuStrip1_Opening;
             // 
-            // toolStripMenuItem1
+            // settingsMenuItem
             // 
-            toolStripMenuItem1.Name = "toolStripMenuItem1";
-            toolStripMenuItem1.Size = new Size(116, 22);
-            toolStripMenuItem1.Text = "Settings";
-            toolStripMenuItem1.Click += onSettingsClick;
+            settingsMenuItem.Name = "settingsMenuItem";
+            settingsMenuItem.Size = new Size(116, 22);
+            settingsMenuItem.Text = "Settings";
+            settingsMenuItem.Click += onSettingsClick;
             // 
-            // toolStripMenuItem2
+            // startMenuItem
             // 
-            toolStripMenuItem2.Name = "toolStripMenuItem2";
-            toolStripMenuItem2.Size = new Size(116, 22);
-            toolStripMenuItem2.Text = "Start";
-            toolStripMenuItem2.Click += onStartClick;
+            startMenuItem.Name = "startMenuItem";
+            startMenuItem.Size = new Size(116, 22);
+            startMenuItem.Text = "Start";
+            startMenuItem.Click += onStartClick;
             // 
-            // toolStripMenuItem3
+            // pauseMenuItem
             // 
-            toolStripMenuItem3.Name = "toolStripMenuItem3";
-            toolStripMenuItem3.Size = new Size(116, 22);
-            toolStripMenuItem3.Text = "Pause";
-            toolStripMenuItem3.Click += onPauseClick;
+            pauseMenuItem.Name = "pauseMenuItem";
+            pauseMenuItem.Size = new Size(116, 22);
+            pauseMenuItem.Text = "Pause";
+            pauseMenuItem.Click += onPauseClick;
             // 
-            // toolStripMenuItem4
+            // restartMenuItem
             // 
-            toolStripMenuItem4.Name = "toolStripMenuItem4";
-            toolStripMenuItem4.Size = new Size(116, 22);
-            toolStripMenuItem4.Text = "Restart";
-            toolStripMenuItem4.Click += onRestartClick;
+            restartMenuItem.Name = "restartMenuItem";
+            restartMenuItem.Size = new Size(116, 22);
+            restartMenuItem.Text = "Restart";
+            restartMenuItem.Click += onRestartClick;
             // 
-            // toolStripMenuItem5
+            // closeMenuItem
             // 
-            toolStripMenuItem5.Name = "toolStripMenuItem5";
-            toolStripMenuItem5.Size = new Size(116, 22);
-            toolStripMenuItem5.Text = "Close";
-            toolStripMenuItem5.Click += onCloseClick;
+            closeMenuItem.Name = "closeMenuItem";
+            closeMenuItem.Size = new Size(116, 22);
+            closeMenuItem.Text = "Close";
+            closeMenuItem.Click += onCloseClick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(26, 49);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(180, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // labelFrom
+            // 
+            labelFrom.AutoSize = true;
+            labelFrom.Location = new Point(26, 22);
+            labelFrom.Name = "labelFrom";
+            labelFrom.Size = new Size(134, 15);
+            labelFrom.TabIndex = 2;
+            labelFrom.Text = "Use transliteration from:";
+            // 
+            // labelTo
+            // 
+            labelTo.AutoSize = true;
+            labelTo.Location = new Point(26, 96);
+            labelTo.Name = "labelTo";
+            labelTo.Size = new Size(23, 15);
+            labelTo.TabIndex = 3;
+            labelTo.Text = "To:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(26, 123);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(180, 23);
+            comboBox2.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.ShowHiddenFiles = true;
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // browseButton
+            // 
+            browseButton.Location = new Point(655, 95);
+            browseButton.Name = "browseButton";
+            browseButton.Size = new Size(75, 23);
+            browseButton.TabIndex = 5;
+            browseButton.Text = "Browse...";
+            browseButton.UseVisualStyleBackColor = true;
+            browseButton.Click += onBrowseClick;
+            // 
+            // browseInput
+            // 
+            browseInput.Location = new Point(289, 95);
+            browseInput.Name = "browseInput";
+            browseInput.ReadOnly = true;
+            browseInput.Size = new Size(372, 23);
+            browseInput.TabIndex = 6;
+            browseInput.Text = Properties.Settings.Default.ConfigPath ?? "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2776, 619);
+            Controls.Add(browseInput);
+            Controls.Add(browseButton);
+            Controls.Add(comboBox2);
+            Controls.Add(labelTo);
+            Controls.Add(labelFrom);
+            Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
-            contextMenuStrip1.ResumeLayout(false);
+            Load += Form1_Load;
+            trayMenu.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         private void onSettingsClick(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Normal;
             ShowInTaskbar = true;
+            jeraKB.IsConfigExists();
+            InitializeLanguages();
         }
 
         private void onStartClick(object sender, EventArgs e)
         {
+            jeraKB.Toggle(true);
             trayIcon.Icon = new Icon("../../../logoOn.ico");
         }
 
         private void onPauseClick(object sender, EventArgs e)
         {
+            jeraKB.Toggle(false);
             trayIcon.Icon = new Icon("../../../logoIdle.ico");
         }
 
@@ -132,16 +233,39 @@ namespace JeraUI
             System.Environment.Exit(0);
         }
 
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private void onBrowseClick(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                Properties.Settings.Default.ConfigPath = file;
+                Properties.Settings.Default.Save();
+                jeraKB.OverridePath(file);
+                browseInput.Text = file;
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+        private System.Windows.Forms.ToolStripMenuItem settingsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeMenuItem;
 
         #endregion
 
         private System.Windows.Forms.ToolStripMenuItem menuItemToolStripMenuItem;
+        private ComboBox comboBox1;
+        private Label labelFrom;
+        private Label labelTo;
+        private ComboBox comboBox2;
+        private OpenFileDialog openFileDialog1;
+        private Button browseButton;
+        private TextBox browseInput;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using JeraKeyboard;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace JeraUI
 {
     internal static class Program
     {
+        static JeraKB jkb;
+        public static IServiceProvider ServiceProvider { get; private set; }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            var jkb = new JeraKeyboardInitializer();
-            jkb.Init();
+            jkb = new JeraKB();
+            jkb.Init(Properties.Settings.Default.ConfigPath);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var host = CreateHostBuilder().Build();
+            ServiceProvider = host.Services;
+
+            Application.Run(ServiceProvider.GetRequiredService<Form1>());
+
+            // Application.Run(new Form1());
+        }
+
+        static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => {
+                    services.AddSingleton<IJeraKB, JeraKB>();
+                    services.AddSingleton<Form1>();
+                });
         }
     }
 }
